e8ea236dedad1d6d83f55f970cd358b1cf31f68b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-02 14:40:34 +0600

    Responsive tiles

---
+++ src/pages/home/ServicesSection.tsx
@@ -0,0 +1,27 @@
+import Container from "@/components/Container";
+
+const ServicesSection = () => {
+  return (
+    <Container className="my-40">
+      <div className="text-center flex flex-col items-center">
+        <h1>Services that we provide.</h1>
+        <p className="max-w-[80ch] mt-10 mb-20">
+          All our technicians are fully qualified and licensed. Moreover,
+          theyare incredibly skillful and proficient in various aspects of
+          computer repair.
+        </p>
+      </div>
+      <div className="grid grid-cols-12 gap-5">
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-12"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-6 lg:col-span-5"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-6  lg:col-span-7"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-12"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-12 lg:col-span-4"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-12 lg:col-span-4"></div>
+        <div className="bg-red-500 h-[415px] rounded-lg col-span-12 lg:col-span-4"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default ServicesSection;
