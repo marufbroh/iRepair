b2192f9cd2690898f43199dc63e6d22628608c2e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-02 12:46:45 +0600

    Fixed navbar animation

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <motion.header className="h-16"
-    
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
+    <motion.header
+      className="h-16 fixed w-full bg-white z-[999]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1 }}
     >
       <nav className="flex justify-between items-center h-full w-full max-w-[1230px] mx-auto px-5">
         <span className="text-3xl">iRepair</span>
