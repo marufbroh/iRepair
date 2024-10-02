5b92f4b79742d68fd392a8d5b04e9bc458439bb1
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-02 15:16:43 +0600

    Custom scroll animation hook

--- src/components/ServiceTiles/BatteryReplacement.tsx
+++ src/components/ServiceTiles/BatteryReplacement.tsx
@@ -1,21 +1,16 @@
-import {motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import useScrollGrow from "@/hooks/useScrollGrow";
+import { motion } from "framer-motion";
 
 const BatteryReplacement = () => {
+const {componentRef, style} = useScrollGrow();
 
-    const componentRef = useRef<HTMLDivElement>(null)
-
-const {scrollYProgress} = useScroll({
-    target: componentRef,
-    offset: ['0 1', '1 1']
-
-});
-
-const scaleValue = useTransform(scrollYProgress, [0, 1], [0.8, 1])
-
-  return <motion.div 
-  style={{scale: scaleValue}}
-  ref={componentRef} className="bg-red-500 h-[415px] rounded-lg col-span-12"></motion.div>;
+  return (
+    <motion.div
+      style={style}
+      ref={componentRef}
+      className="bg-red-500 h-[415px] rounded-lg col-span-12"
+    ></motion.div>
+  );
 };
 
 export default BatteryReplacement;
--- src/pages/home/ServicesSection.tsx
+++ src/pages/home/ServicesSection.tsx
@@ -1,5 +1,7 @@
 import Container from "@/components/Container";
 import BatteryReplacement from "@/components/ServiceTiles/BatteryReplacement";
+import ChipsetReplacement from "@/components/ServiceTiles/ChipsetReplacement";
+import DataRecovery from "@/components/ServiceTiles/DataRecovery";
 
 const ServicesSection = () => {
   return (
@@ -14,8 +16,8 @@ const ServicesSection = () => {
       </div>
       <div className="grid grid-cols-12 gap-5">
         <BatteryReplacement/>
-        <div className="bg-red-500 h-[415px] rounded-lg col-span-6 lg:col-span-5"></div>
-        <div className="bg-red-500 h-[415px] rounded-lg col-span-6  lg:col-span-7"></div>
+        <ChipsetReplacement/>
+        <DataRecovery/>
         <div className="bg-red-500 h-[415px] rounded-lg col-span-12"></div>
         <div className="bg-red-500 h-[415px] rounded-lg col-span-12 lg:col-span-4"></div>
         <div className="bg-red-500 h-[415px] rounded-lg col-span-12 lg:col-span-4"></div>
