ce4fbb65836c15afac3ab5d4ca5bf4e4b6dff606
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-02 09:20:05 +0600

    iRepair initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Hello Button</Button>
+    </>
+  );
+}
+
+export default App;
