0576cafc8588c9c442743aa766d6270f0c880605
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-02 09:40:21 +0600

    Shadcn and React Router Dom configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Hello Button</Button>
-    </>
-  );
+  return <MainLayout />;
 }
 
 export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,21 @@
+import App from "@/App";
+import About from "@/pages/About";
+import Home from "@/pages/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
